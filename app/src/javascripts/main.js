c1172a8a8b3dfe9a650c4f8d847bb1704d45bd64
listencx <664267693@qq.com>
2016-12-30 08:42:51 +0800

    微调样式

--- app/src/javascripts/main.js
+++ app/src/javascripts/main.js
@@ -1,6 +1,63 @@
 (function () {
     'use strict';
 
+    $(function(){
+
+    var name = decodeURIComponent(getParams("u"));
+    //alert(name);
+    if(name != "null"){
+
+        $.get("http://nh2016.auxgroup.com/party/life/employee/"+name).then(function(response) {
+            var userId=response.userId;
+            var startDate=response.startDate;
+            var workdays=response.workdays;
+            var birthplace=response.birthplace;
+            var beat=response.beat;
+            var villagers=response.villagers;
+            document.title=userId+'的奥克斯人生';
+            if(beat==100){
+                $(".item-text-beat").css("font-size","2.0rem");
+                $(".item-text-beat").css("top","64.8%");
+                $(".item-text-beat").css("left","40.2%");
+            }
+            if(beat.length<2)beat="&nbsp"+beat;
+            var arr=startDate.split("-");
+            var year=arr[0];
+            var month=arr[1];
+            var day=arr[2]; 
+            $(".item-year").html(year);
+            $(".item-month").html(month);
+            $(".item-day").html(day);
+            $(".item-birthplace").html(birthplace);
+            $(".item-villagers").html(villagers);
+
+            if(workdays.length <= 4){        
+            for (var i = 1; i <= 4 ; i++) {
+                    $(".item-text-howlong-"+i).html(workdays[i-1]);
+                }
+            }
+            else{
+                $("#number-four").css("display","none");
+                $("#number-five").css("display","inline");
+                for (var i = 1; i <= 5 ; i++) {
+                    $(".item-text-howlong-5-"+i).html(workdays[i-1]);
+                }
+            }
+            $(".item-text-beat").html(beat+"%");
+         },function(response){
+            console.log(response);
+         })
+        }
+    })
+
+    function getParams(name){
+            var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);
+            if(r!=null){
+                return r[2];
+            }
+            return null;
+        }
     // load dependencies
     var animationControl = require('./animation-control.js');
 
@@ -20,21 +77,27 @@
                 $(this).addClass('paused');
             }
         });
+        $('.line').click(function(){
+            mySwiper.slideNext();
+        })
+        $('.slide-1').click(function(){
+            mySwiper.slideNext();
+        })
 
         // init Swiper
-        new Swiper('.swiper-container', {
+        var mySwiper =new Swiper('.swiper-container', {
             mousewheelControl: true,
             effect: 'coverflow',    // slide, fade, coverflow or flip
             speed: 400,
-            direction: 'vertical',
+            direction: 'vertical',autoHeight: true, 
             fade: {
                 crossFade: false
             },
             coverflow: {
-                rotate: 100,
+                rotate: 0,
                 stretch: 0,
-                depth: 300,
-                modifier: 1,
+                depth: 0,
+                modifier: 0,
                 slideShadows: false     // do disable shadows for better performance
             },
             flip: {
@@ -45,13 +108,13 @@
                 animationControl.initAnimationItems();  // get items ready for animations
                 animationControl.playAnimation(swiper); // play animations of the first slide
             },
-            onTransitionStart: function (swiper) {     // on the last slide, hide .btn-swipe
-                if (swiper.activeIndex === swiper.slides.length - 1) {
-                    $upArrow.hide();
-                } else {
-                    $upArrow.show();
-                }
-            },
+            // onTransitionStart: function (swiper) {     // on the last slide, hide .btn-swipe
+            //     if (swiper.activeIndex === swiper.slides.length - 1) {
+            //         $upArrow.hide();
+            //     } else {
+            //         $upArrow.show();
+            //     }
+            // },
             onTransitionEnd: function (swiper) {       // play animations of the current slide
                 animationControl.playAnimation(swiper);
             },
